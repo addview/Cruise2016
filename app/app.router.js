07340da9f4d08898a96773a3693d8ae01fbdd59b
addview <fredrik@addview.se>
2015-07-17 07:26:52 +0200

    multi views
    Add multi views

--- app/app.router.js
+++ app/app.router.js
@@ -7,16 +7,32 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         
         // HOME STATES AND NESTED VIEWS ========================================
+        // .state('home', {
+        //     url: '/home',
+        //     templateUrl: 'app/components/home/homeView.html',
+        //     controller: 'homeController'
+        // })
+
         .state('home', {
             url: '/home',
-            templateUrl: 'app/components/home/homeView.html',
-            controller: 'homeController'
+            views: {
+                '': { templateUrl: 'app/components/home/homeView.html' },
+                'columnOne@home': { 
+                    templateUrl: 'app/components/search/searchView.html',
+                    controller: 'searchController'
+                },
+                'columnTwo@home': { 
+                    templateUrl: 'app/components/list/listView.html',
+                    controller: 'listController'
+                }
+            }
+            
         })
 
         .state('about', {
             url: '/about',
             templateUrl: 'app/components/about/aboutView.html',
             controller: 'aboutController'
-        })      
+        });   
         
 });
---
+++ app/components/search/searchController.js
@@ -0,0 +1,5 @@
+var app = angular.module('app');
+
+app.controller('searchController', function($scope) {
+    $scope.message = 'Hello Fredrik it´s running, tjing on search';
+});
